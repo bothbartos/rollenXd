c0b597d90dd8e3d69462a5eaa9dc22448d37ba32
Erdős Marcell <erdosmarci@gmail.com>
2025-02-18 11:29:25 +0100

    make username unique

--- backend/populate/create_tables.py
+++ backend/populate/create_tables.py
@@ -60,7 +60,7 @@ try:
     -- USERS Table
     CREATE TABLE User_Table (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
+        name VARCHAR(255) UNIQUE NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         profile_picture BYTEA,
