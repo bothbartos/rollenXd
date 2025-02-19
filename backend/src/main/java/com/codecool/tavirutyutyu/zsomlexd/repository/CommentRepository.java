c91aa18530860626fe1f2a2cf9121eff5583f06e
bartosboth <bothbartos@gmail.com>
2025-02-19 13:54:26 +0100

    updated endpoint to use dto

--- backend/src/main/java/com/codecool/tavirutyutyu/zsomlexd/repository/CommentRepository.java
+++ backend/src/main/java/com/codecool/tavirutyutyu/zsomlexd/repository/CommentRepository.java
@@ -1,13 +1,11 @@
 package com.codecool.tavirutyutyu.zsomlexd.repository;
 
 import com.codecool.tavirutyutyu.zsomlexd.model.Comment;
-import com.codecool.tavirutyutyu.zsomlexd.model.comment.CommentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
