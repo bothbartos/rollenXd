baccc4b572e70350b680199073040c362a4ff717
bartosboth <bothbartos@gmail.com>
2025-03-06 11:35:02 +0100

    updated song player css

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -12,7 +12,7 @@ export default function ResultPage() {
     const [searchParams] = useSearchParams();
     const searchQuery = searchParams.get('query') || '';
 
-    const { data, error, isLoading } = useQuery({
+    const { data: songs, error, isLoading } = useQuery({
         queryKey: ['searchSongs', searchQuery],
         queryFn: () => searchSongs(searchQuery),
         enabled: !!searchQuery
@@ -20,22 +20,31 @@ export default function ResultPage() {
 
     if (isLoading) return <p>Searching songs...</p>;
     if (error) return <p>Error: {error.message}</p>;
-    if (!data?.data?.length) return <p>No results found</p>;
-    data.data.forEach((song) => {
-        console.log(song.coverBase64)
-    })
-
+    if (!songs?.data?.length) return (
+        <div className="h-full p-4 space-y-8">
+            {/* Songs Section */}
+            <div className="w-full">
+                <h1 className="text-2xl font-semibold mb-1 ml-10 text-white">No results </h1>
+            </div>
+        </div>
+    );
     return (
-        <div className="w-full overflow-x-auto scrollbar-hide">
-            <div className="flex flex-nowrap pl-4">
-                {data.data.map((song) => (
-                    <div key={song.title} className="flex-shrink-0 mr-4 last:mr-0 m-10">
-                        <MediaElement
-                            item={song}
-                            type="song"
-                        />
+        <div className="h-full p-4 space-y-8">
+            {/* Songs Section */}
+            <div className="w-full">
+                <h1 className="text-2xl font-semibold mb-1 ml-10 text-white">Song results:</h1>
+                <div className="w-full overflow-x-auto scrollbar-hide">
+                    <div className="flex flex-nowrap pl-4">
+                        {songs.data.map((song) => (
+                            <div
+                                key={song.title}
+                                className="flex-shrink-0 mr-4 last:mr-0 m-10"
+                            >
+                                <MediaElement item={song} type="song"/>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
