da77a2d465c55e0669e053c706a696fe04e00bf5
bartosboth <bothbartos@gmail.com>
2025-03-20 14:50:36 +0100

    added div colouring functionality for playlist details

--- frontend/src/components/PlaylistSongs.jsx
+++ frontend/src/components/PlaylistSongs.jsx
@@ -1,9 +1,21 @@
 import {convertDoubleToMinuteSecond} from "../utils/Utils.js";
+import {useContext, useEffect, useState} from "react";
+import {PlayerContext} from "../context/PlayerContext.jsx";
 
 export default function PlaylistSongs({ song }) {
+    const {currentSong} = useContext(PlayerContext)
+    const [divColor, setDivColor] = useState("bg-gray-800")
+    useEffect(() => {
+        console.log("playlistSongs")
+        if (currentSong && currentSong.id === song.id) {
+            setDivColor("bg-blue-700")
+        }else{
+            setDivColor("bg-gray-800")
+        }
+    }, [currentSong])
 
     return (
-        <div className={`flex items-center justify-between bg-gray-800 p-3 rounded-md`}>
+        <div className={`flex items-center justify-between ${divColor} bg-blue p-3 rounded-md`}>
             <div className="flex items-center space-x-4">
                 <img
                     src={`data:image/png;base64,${song.coverBase64}`}
